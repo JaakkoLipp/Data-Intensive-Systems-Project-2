479a23555209bb7c5efb287ada712fa89c0ecb81
Jaakko <jaakko.lipponen01@gmail.com>
2024-11-16 11:37:04 +0200

    UI file added

---
+++ frontend.py
@@ -0,0 +1,16 @@
+##############################################
+# Frontend code here, this file could then be imported into main.py as a library.
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+##############################################
+
+def userinterface():
+    print("Welcome to the User Interface!")
+
+if __name__ == "__main__":
+    userinterface()
